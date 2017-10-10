d343c335fbf451ff1eebe3b347954c401c22ebf1
Panu Matilainen <pmatilai@redhat.com>
2017-10-10 11:44:10 +0300

    Place file signatures into the signature header where they belong
    The original file signing puts the file signatures into the main header
    immutable region, invalidating all previous signatures and digests so
    the package no longer appears to be what it was when it came out of the
    assembly line. Which is bad. Doing that also requires recalculating
    everything again which is just added complexity, and since it adds
    stuff to different place from the rest of the signing, it requires yet
    complexity to deal with that. Moving the file signatures into the
    signature header solves all that and allows removing a big pile of
    now unnecessary code.
    
    Because this means retrofitting tags bass-ackwards into the signature
    header, the tag definitions are backwards to everything else. Other
    options would certainly be possible, but this makes things look more
    normal on the signature header side. "Users" only ever see the
    unchanged file signature tags as they have always been.
    
    This also means the signature header can be MUCH bigger than ever before,
    so bump up the limit (to 64MB, arbitrary something for now), and
    permit string array types to be migrated from the signature header
    on package read.
    
    Caveats:
    This loses the check for identical existing signatures to keep the
    complexity down, it's hardly a critical thing and can be added back later.
    While file signing could now be done separately to other signing, that
    is not handled here.

--- lib/header.c
+++ lib/header.c
@@ -1910,7 +1910,7 @@ rpmRC hdrblobRead(FD_t fd, int magic, int exact_size, rpmTagVal regionTag, hdrbl
 
     if (regionTag == RPMTAG_HEADERSIGNATURES) {
 	il_max = 32;
-	dl_max = 8192;
+	dl_max = 64 * 1024;
     }
 
     memset(block, 0, sizeof(block));
